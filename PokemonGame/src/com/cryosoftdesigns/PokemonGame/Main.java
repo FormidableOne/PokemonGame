8aff267e95ad8cff99fa19c6ae33280d1630c307
KJ <kjbrackish@gmail.com>
2014-12-19 03:55:34 -0700

    Initialization of Pokemon Game Repo

---
+++ PokemonGame/src/com/cryosoftdesigns/PokemonGame/Main.java
@@ -0,0 +1,16 @@
+package com.cryosoftdesigns.PokemonGame;
+
+/**
+ * @author KJ
+ */
+public class Main {
+
+	public static void main(String[] args) {
+	
+		System.out.println("This is a test program!\nThis is only a test!");
+		System.out.println("I am attempting to check the Git functionality of Eclipse.");
+		System.out.println("If all goes well, I should be able to use Eclipse as my one-stop-shop.");
+		
+	}
+
+}
